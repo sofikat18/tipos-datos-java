0f0f8ece08dfc07c9058724f61f6680104916066
Sofia Lopez <anasolori.92@gmail.com>
2021-07-01 11:34:14 -0500

    Primer commit proyecto Java

---
+++ ConversionDatos/src/ConversionDatos.java
@@ -0,0 +1,28 @@
+
+public class ConversionDatos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Tipos de datos en Java
+		 * Numéricos: byte, short, int, long, float, double
+		 * Booleanos: boolean
+		 * Caracteres: char
+		 * Cadenas: String
+		 */
+		
+		byte numeropequeno = 122;
+		byte otronumeropequeno = 122;
+		int suma = numeropequeno + otronumeropequeno;
+		
+		int numero1 = 1000;
+		long numero2 = numero1;
+		float numero3 = numero2;
+		
+		System.out.println(numero1);
+		System.out.println(numero2);
+		System.out.println(numero3);
+		System.out.println(suma);
+	}
+
+}
